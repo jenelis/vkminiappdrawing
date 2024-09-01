cb20ca2f18c6838587157af9e1e62b512231abc2
jenelis <beutifel4a@gmail.com>
2024-09-01 13:27:42 +0900

    Обнова

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const closePopup = document.getElementById('closePopup');
     const resultMessage = document.getElementById('resultMessage');
 
+    const dataToSend = {
+        username: 'exampleUser',  // Пример данных
+        email: 'example@example.com'  // Пример данных
+    };
+
+    const queryParams = new URLSearchParams(dataToSend).toString();
+
+    // Выполняем GET запрос с параметрами
+    fetch(`/register_users?${queryParams}`, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            resultMessage.textContent = `Результат: ${JSON.stringify(data)}`;
+            resultPopup.style.display = 'block';
+        })
+        .catch(error => {
+            resultMessage.textContent = `Ошибка: ${error}`;
+            resultPopup.style.display = 'block';
+        });
+
     registerBtn.addEventListener('click', function() {
-        fetch('/register_users')
-            .then(response => response.json())
-            .then(data => {
-                resultMessage.textContent = `Результат: ${JSON.stringify(data)}`;
-                resultPopup.style.display = 'block';
-            })
-            .catch(error => {
-                resultMessage.textContent = `Ошибка: ${error}`;
-                resultPopup.style.display = 'block';
-            });
+
     });
 
     closePopup.addEventListener('click', function() {
